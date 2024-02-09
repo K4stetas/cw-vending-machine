ee59329254a85f907a7aeedecff07049ff05c5cd
vboxuser <vboxuser@cosmwasm.myguest.virtualbox.org>
2024-02-09 18:39:01 +0100

    Empty contract to work with

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use cosmwasm_std::{
+    entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+mod contract;
+mod error;
+mod msg;
+mod state;
+
+// --snip--
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    contract::instantiate(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg)
+ -> StdResult<Binary>
+{
+    contract::query(deps, env, msg)
+}
+
+#[entry_point]
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
+    contract::execute(deps, env, info, msg)
+}
